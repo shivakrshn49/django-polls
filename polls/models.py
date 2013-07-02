49211a826d142401c237ca7a62a3c3cc41c2741d
shivakrishnab <shiva@agiliq.com>
2013-07-02 19:25:02 +0530

    Modified Choice model and added Vote Model

--- polls/models.py
+++ polls/models.py
@@ -26,9 +26,13 @@ class Poll(models.Model):
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
-    choice_user = models.ForeignKey(User)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __unicode__(self):
     	return self.choice_text
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, related_name="user_votes", null=True, blank=True)
+    poll = models.ForeignKey(Poll)
+    choice = models.ForeignKey(Choice, related_name='selected_choice')        
--- polls/urls.py
+++ polls/urls.py
@@ -4,15 +4,6 @@ from polls import views
 
 
 urlpatterns = patterns('',
-    # ex: /polls/
-    # url(r'^$', views.index, name='index'),
-    # # ex: /polls/5/
-    # url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    # # ex: /polls/5/results/
-    # url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    # # ex: /polls/5/vote/
-    # url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
-
     url(r'^$', login_required(views.IndexView.as_view()), name='index'),
     url(r'^(?P<pk>\d+)/$', login_required(views.DetailView.as_view()), name='detail'),
     url(r'^(?P<pk>\d+)/results/$', login_required(views.ResultsView.as_view()), name='results'),
