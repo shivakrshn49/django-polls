3007476b1757094ff2524e6214639c9e55ddca36
shivakrishnab <shiva@agiliq.com>
2013-07-04 18:07:55 +0530

    Changed settings.py file and added colectstatic files

--- django_polls/settings.py
+++ django_polls/settings.py
@@ -66,7 +66,9 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+# STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH,'../staticfiles')
+print STATIC_ROOT
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -74,7 +76,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-            os.path.join(PROJECT_PATH,'../static'),
+            # os.path.join(PROJECT_PATH,'../static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
