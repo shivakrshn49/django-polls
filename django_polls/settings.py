60c7bd572beaa7032410d2c73daa78bc87b2ec88
shivakrishnab <shiva@agiliq.com>
2013-07-04 15:20:12 +0530

    Modified settings.py

--- django_polls/settings.py
+++ django_polls/settings.py
@@ -171,3 +171,10 @@ try:
     from localsettings import *
 except ImportError, e:
     print 'Unable to load localsettings.py:', e
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')    
+print os.getcwd()
