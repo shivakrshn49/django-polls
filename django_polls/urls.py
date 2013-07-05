e667f67846f815ed86063563a8428ba77c02128f
shivakrishnab <shiva@agiliq.com>
2013-07-05 13:04:19 +0530

    modified:   django_polls/urls.py

--- django_polls/urls.py
+++ django_polls/urls.py
@@ -19,6 +19,6 @@ if not settings.DEBUG:
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
 
-# urlpatterns += patterns('',
-#         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-#     )    
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )    
