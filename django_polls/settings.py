80bb68d3c103bb170fa390fe98ecf939bd5cf2a4
shivakrishnab <shiva@agiliq.com>
2013-07-05 12:56:13 +0530

    modified:   django_polls/settings.py

--- django_polls/settings.py
+++ django_polls/settings.py
@@ -1,7 +1,7 @@
 # Django settings for django_polls project.
 
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 import os
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
